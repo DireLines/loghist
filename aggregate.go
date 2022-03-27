5564953e9b2b1b49f2abe3b893ad2bf6be0bc0b4
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2022-03-27 17:31:10 -0400

    yep

---
+++ aggregate.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	data := map[string][]int{}
+	go func() {
+		for {
+			line, _ := reader.ReadString('\n')
+			trimmed := strings.TrimSpace(line)
+			words := strings.Split(trimmed, " ")
+			if words[len(words)-1] == "micros" || words[len(words)-1] == "millis" {
+				key := strings.Join(words[:len(words)-3], " ")
+				val, err := strconv.Atoi(words[len(words)-2])
+				if err != nil {
+					continue
+				}
+				data[key] = append(data[key], val)
+			}
+		}
+	}()
+
+	for {
+		time.Sleep(time.Millisecond * 500)
+		json, _ := json.Marshal(data)
+		fmt.Println(string(json))
+		data = map[string][]int{}
+	}
+}
--- hist.py
+++ hist.py
@@ -1,20 +1,33 @@
 import sys
-import asyncio
-import subprocess
+import json
 from matplotlib import pyplot as plt
 
-indicator = sys.argv[1]
-lines = []
+filters = sys.argv[1:]
+def filterDict(predicate,dictObj):
+    result = {}
+    for (key, value) in dictObj.items():
+        if predicate(key, value):
+            result[key] = value
+    return result
+def filters_contain_key(key,value):
+    for filter in filters:
+        if filter in key:
+            return True
+    return False
+data = {}
+plt.ion()
+plt.show()
 while True:
-    num_lines = 5000
-    lines.extend(sys.stdin.readlines(num_lines))
-    plt.hist(
-        list(
-            map(float,#convert to number
-                map(lambda s: s.split()[-2],#extract data point
-                    filter(lambda s: indicator in s, lines)
-                )
-            )
-        )
-    )
-    plt.show()
+    batch = json.loads(sys.stdin.readline())
+    if filters:
+        batch = filterDict(filters_contain_key,batch)
+    for k in batch:
+        if k not in data:
+            data[k] = []
+        data[k].extend(batch[k])
+    nums = data.values()
+    plt.cla()
+    plt.hist(nums,30,stacked=True)
+    plt.legend(data.keys())
+    plt.draw()
+    plt.pause(0.001)
